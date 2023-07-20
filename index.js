b2573569f1da21501a17fc82b40449ad018cb395
nainoktv <nainoaktvillegas@gmail.com>
2023-07-19 23:37:39 -0700

    add event listener for player choice and create computerTurn function

--- index.js
+++ index.js
@@ -12,3 +12,26 @@ let computer;
 
 // Display who won
 let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+
+  player = button.textContent;
+  computerTurn();
+}));
+
+function computerTurn() {
+
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch(randNum) {
+    case 1:
+      computer = "ROCK";
+      break;
+    case 2:
+      computer = "PAPER";
+      break;
+    case 3:
+      computer = "SCISSORS";
+      break;
+  }
+}
