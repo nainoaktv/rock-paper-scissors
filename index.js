35ebee250a8ac3fcf05fdef6883e200a1050b4d8
nainoktv <nainoaktvillegas@gmail.com>
2023-07-19 23:32:04 -0700

    utilize query selector and create variables to store choices

---
+++ index.js
@@ -0,0 +1,14 @@
+// Selecting the results text by ID
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+
+// Selecting the choice buttons by class
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+
+// Create 2 variables to store player and computer choice
+let player;
+let computer;
+
+// Display who won
+let result;
